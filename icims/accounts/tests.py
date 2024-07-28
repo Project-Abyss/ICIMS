3e4daf8c98d3a202909835f8df16da84d8aa6a3d
bessyhuang <bessyhuang1@gmail.com>
2024-07-28 10:30:12 +0800

    Test: test MongoDB atlas connection

--- icims/accounts/tests.py
+++ icims/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.conf import settings
+from decouple import Config, RepositoryEnv
+
+from pymongo import MongoClient
+import os
+
+
+base_dir = os.getcwd()
+ENV = Config(RepositoryEnv(base_dir + '/.env'))
 
 # Create your tests here.
+class TestDatabase(TestCase):
+    def test_db_query(self):
+        dbClient = MongoClient(settings.DATABASES['default']['CLIENT']['host'])
+        db = dbClient[settings.DATABASES['default']['NAME']]
+        COLLECTION_NAME = 'django_migrations'
+        result = db[COLLECTION_NAME].find_one()
+        print(result)
