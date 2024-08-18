ddc8c231ceb664ef9bc66f02af402d2d9ead3824
bessyhuang <bessyhuang1@gmail.com>
2024-08-18 23:33:12 +0800

    Feat (Database): DB schema set the ForeignKey null

--- icims/accounts/models.py
+++ icims/accounts/models.py
@@ -13,12 +13,12 @@ ROLE_CHOICES = (
 
 class Data_user(models.Model):
     _id = models.ObjectIdField(primary_key=True, default=ObjectId)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=100, blank=False)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    email = models.EmailField(max_length=200)
-    department = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    department = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=False, blank=False)
 
     def is_administrator(self):
         return self.role == 'administrator'
@@ -32,20 +32,20 @@ class Data_user(models.Model):
 
 class Enterprise(models.Model):
     _id = models.ObjectIdField(primary_key=True, default=ObjectId)
-    full_name = models.CharField(max_length=100, blank=False)
-    address = models.CharField(max_length=100, blank=False)
-    telephone = models.CharField(max_length=100, blank=False)
+    full_name = models.CharField(max_length=100, null=False, blank=False)
+    address = models.CharField(max_length=100, null=False, blank=False)
+    telephone = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
         return self.full_name
 
 class Internship(models.Model):
     _id = models.ObjectIdField(primary_key=True, default=ObjectId)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    academic_year = models.CharField(max_length=100, blank=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.SET_NULL, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    academic_year = models.CharField(max_length=100, null=False, blank=True)
 
     def __str__(self):
         return F"{self.academic_year} | {self.user}【{self.enterprise}】"
--- icims/journals/models.py
+++ icims/journals/models.py
@@ -9,21 +9,21 @@ from bson import ObjectId
 # Create your models here.
 class Journal(models.Model):
     _id = models.ObjectIdField(primary_key=True, default=ObjectId)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    last_updated = models.DateField(auto_now=True)
-    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=50, null=False, blank=False)
+    last_updated = models.DateField(auto_now=True, null=False, blank=True)
+    content = models.TextField(null=False, blank=False)
 
     def __str__(self):
         return F"{self.title} | {self.user}"
 
 class Comment(models.Model):
     _id = models.ObjectIdField(primary_key=True, default=ObjectId)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
-    last_updated = models.DateField(auto_now=True)
-    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    journal = models.ForeignKey(Journal, on_delete=models.SET_NULL, null=True, blank=True)
+    last_updated = models.DateField(auto_now=True, null=False, blank=True)
+    content = models.TextField(null=False, blank=False)
 
     def __str__(self):
         return F"{self.journal} | {self.user}"
